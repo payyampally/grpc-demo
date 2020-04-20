b6f76c031a6ce4bd1c9e9423652b86bde8dd4801
Denys Payyampally <payyampally@gmail.com>
2020-04-19 21:33:23 -0400

    Added grpc-demo project

---
+++ src/test/java/com/zafin/spring/grpc/GrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zafin.spring.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
